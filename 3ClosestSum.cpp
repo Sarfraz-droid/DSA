3a50e0efcf15a61c19e0fb5f3969280537cac7f7
Sarfraz-droid <73013838+Sarfraz-droid@users.noreply.github.com>
2021-04-24 15:12:31 +0530

    3Sum

---
+++ 3ClosestSum.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	vector<int> A(n);
+	int diff = INT_MAX;
+	int maxsum;
+	int target;
+	cin>>target;
+	for(int i = 0;i<n;i++)
+	{
+		int p;
+		cin>>p;
+		A[i] = p;
+	}
+
+    sort(A.begin(),A.end());
+    int closeDiff=INT_MAX;
+    int close_sum;
+    for(int i=0;i<n-2;i++)
+    {
+        int sum=0;
+        int j=i+1,k=n-1;
+
+        while(j<k)
+        {
+			sum = A[i] + A[j] + A[k];
+            if(abs(sum-target)<closeDiff)
+            {
+                closeDiff=abs(sum-target);
+                close_sum=sum;
+            }
+            if(sum<target)
+            {
+                j++;
+            }
+            else if(sum>target)
+            {
+                k--;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+	cout<<close_sum;
+	return 0;
+}
